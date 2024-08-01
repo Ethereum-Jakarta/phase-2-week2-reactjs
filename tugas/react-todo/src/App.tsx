0eac6ce0e52630e16e4370f3f170b39a2a3a551b
Rakemoon <rakemoonkuzmin@gmail.com>
2024-08-01 19:57:14 +0700

    push: React Todo Lifecycle

--- tugas/react-todo/src/App.tsx
+++ tugas/react-todo/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import AddTask from "./components/AddTask";
 import TodoList from "./components/TodoList";
@@ -10,8 +10,9 @@ export type Task = {
   date: Date;
 }
 
-function App() {
+const App: React.FC<{}> = () => {
   const [tasks, setTasks] = useState([] as Task[]);
+  const [isFetched, setIsFetched] = useState(false);
 
   const addTask = (task: string) => {
     console.log(task);
@@ -23,6 +24,39 @@ function App() {
   ));
 
   const deleteTask = (id: number) => setTasks(tasks.filter(task => task.id !== id));
+
+  /**
+   * INFO: Untuk ini saya bikin sendiri ya sir.
+   * soalnya yang ditutor kalo tasks dihapus semua
+   * saat refresh malah muncul lagi.
+   */
+  useEffect(() => {
+    if (!isFetched) {
+      const prevTasks: Task[] = JSON.parse(localStorage.getItem("tasks") ?? "[]");
+      setTasks(prevTasks.map(x =>
+        /** 
+         * INFO: parse the iso string into Date
+         * because when you saving the tasks
+         * the Date object become isoString
+         */
+        ({...x, date: new Date(x.date)})
+      ));
+      setIsFetched(true);
+    } else {
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
+  }, [tasks, isFetched]);
+
+  // useEffect(() => {
+  //   if (!tasks.length) return;
+  //   localStorage.setItem("tasks", JSON.stringify(tasks));
+  // }, [tasks]);
+  //
+  // useEffect(() => {
+  //   const previousTasks = JSON.parse(localStorage.getItem("tasks") ?? "[]") as Task[];
+  //   setTasks(previousTaskse(x.date))
+  // ));
+  // }, []);
 
   return (
     <div className="App">
