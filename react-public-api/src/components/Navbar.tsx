626fdc7629452bc0dfccc5728a9bf58c3b51821e
RaihanArdianata <raihan.ardianata@gmail.com>
2024-08-12 21:04:28 +0700

    add naruto font

--- react-public-api/src/components/Navbar.tsx
+++ react-public-api/src/components/Navbar.tsx
@@ -2,9 +2,10 @@ import logo from '@/assets/akatsuki.png';
 
 function Navbar() {
   return (
-    <div>
-      <div>
-        <img src={logo} />
+    <div className="h-16 w-full px-10 py-2 flex font-custom">
+      <div className="flex flex-row items-center gap-4">
+        <img src={logo} alt="logo" className="w-10 h-10" />
+        <p>Akatsuki</p>
       </div>
       <div></div>
     </div>
