8a07a49acda7cfa8573667ec56586a14c427d2b8
RaihanArdianata <raihan.ardianata@gmail.com>
2024-08-13 15:34:04 +0700

    add logo blue archive

--- react-public-api/src/components/Navbar.tsx
+++ react-public-api/src/components/Navbar.tsx
@@ -1,11 +1,10 @@
-import logo from '@/assets/akatsuki.png';
+import logo from '@/assets/logo.png';
 
 function Navbar() {
   return (
-    <header className="h-16 w-full px-10 py-2 flex font-custom sticky top-0 z-50">
+    <header className="h-16 w-full px-10 py-2 flex font-custom sticky top-0 z-20">
       <div className="flex flex-row items-center gap-4">
-        <img src={logo} alt="logo" className="w-10 h-10" />
-        <p className="text-[#ab1b21]">Akatsuki</p>
+        <img src={logo} alt="logo" className="w-full h-full" />
       </div>
       <div></div>
     </header>
