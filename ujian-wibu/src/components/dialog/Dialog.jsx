7320104606bb8bc327132745df6d98e03181c69e
yogiwijaya777 <yogiwijaya115@gmail.com>
2024-09-24 06:53:43 +0700

    Adding time component

--- ujian-wibu/src/components/dialog/Dialog.jsx
+++ ujian-wibu/src/components/dialog/Dialog.jsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
 import CloseDialog from "../button/CloseDialog";
+import Timer from "../Timer";
 
 const Dialog = ({ isOpen, setIsOpen }) => {
   const [isOpen2, setIsOpen2] = useState(false);
 
   return (
     <div className="bg-white rounded h-[65%] w-[50%] z-10 fixed drop-shadow-md">
+      <Timer />
       <button
         onClick={() => setIsOpen2(true)}
-        className="bg-gradient-to-r absolute top-2 right-2 from-violet-600 to-indigo-600 text-white font-medium px-4 py-2 rounded hover:opacity-90 transition-opacity"
+        className="bg-gradient-to-r absolute top-3 right-2 from-violet-600 to-indigo-600 text-white font-medium px-4 py-2 rounded hover:opacity-90 transition-opacity"
       >
         X
       </button>
