f402e77685bda5df9b8fcbf1f4aab4666f7a1c2a
yogiwijaya777 <yogiwijaya115@gmail.com>
2024-09-24 04:22:38 +0700

    Init project

---
+++ ujian-wibu/src/App.jsx
@@ -0,0 +1,11 @@
+import { GridHoverHero } from "./components/hero/Hero";
+
+const App = () => {
+  return (
+    <div className="App">
+      <GridHoverHero />
+    </div>
+  );
+};
+
+export default App;
---
+++ ujian-wibu/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./pages/**/*.{html,jsx}", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
